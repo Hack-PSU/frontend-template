b8b8ea05918fc7aec3485702fe1ccb9b30c9bd1a
ChaseRensberger <chasehrensberger@gmail.com>
2023-02-24 15:07:10 -0500

    add sign-in-modal although some of it should maybe be redone with radix/framer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,26 +2,31 @@ import Image from "next/image";
 import Logo from "../../assets/HackPSUBWLogo1.png";
 import { motion } from "framer-motion";
 import useScroll from "@/lib/hooks/use-scroll";
+import { useSignInModal } from "./sign-in-modal";
 
 const Navbar = () => {
 	const scrolled = useScroll(50);
+	const { SignInModal, setShowSignInModal } = useSignInModal();
 
 	return (
-		<div
-			className={`fixed top-0 w-full flex flex-row items-center p-2 px-16 justify-between ${
-				scrolled
-					? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-					: "bg-white/0"
-			} z-30 transition-all`}
-		>
-			<Image src={Logo} width={50} height={50} alt="logo" />
-			<motion.button
-				className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-				onClick={() => console.log("Sign In")}
+		<>
+			<SignInModal />
+			<div
+				className={`fixed top-0 w-full flex flex-row items-center p-2 px-16 justify-between ${
+					scrolled
+						? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+						: "bg-white/0"
+				} z-30 transition-all`}
 			>
-				Sign In
-			</motion.button>
-		</div>
+				<Image src={Logo} width={50} height={50} alt="logo" />
+				<motion.button
+					className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+					onClick={() => setShowSignInModal(true)}
+				>
+					Sign In
+				</motion.button>
+			</div>
+		</>
 	);
 };
 
