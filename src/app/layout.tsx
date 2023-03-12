57ea58685af51085f540ae47072c1527540b9202
susanto-tm <susanto.tm@gmail.com>
2023-03-12 19:14:17 -0400

    Added FirebaseProvider with integration to signup, login, logout, manage user authentication state, and authentication error. Added ModalsProvider to manage multiple modal states and register nested modal components to page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 "use client";
 import "./globals.css";
+import React from "react";
 import Navbar from "@/components/Navbar";
+import FirebaseProvider from "@/lib/providers/FirebaseProvider";
+import { auth } from "@/lib/config";
 
 export default function RootLayout({
 	children,
@@ -13,10 +16,12 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-			<head />
 			<body>
-				<Navbar />
-				{children}
+			<head />
+				<FirebaseProvider auth={auth}>
+					<Navbar />
+					{children}
+				</FirebaseProvider>
 			</body>
 		</html>
 	);
