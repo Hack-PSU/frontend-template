3d7e63324e644ea653d9bd23e209a9f9d2c300c8
Space-Robo <yash2raj.yr@gmail.com>
2023-03-13 10:30:48 -0400

    Added Sponsors template

--- src/components/sections/Sponsors.tsx
+++ src/components/sections/Sponsors.tsx
@@ -1,4 +1,24 @@
 import Divider from "../Divider";
+import Image from "next/image";
+
+import NAISS_placeholder_img from '../../../assets/sponsors/naiss_placeholder.svg'
+import NAISS_placeholder_img2 from '../../../assets/sponsors/naiss_placeholder.svg'
+
+
+const sponsors = [
+	{
+	  id: 1,
+	  name: 'NAISS',
+	  image: NAISS_placeholder_img,
+	  website: 'https://nittanyai.psu.edu/',
+	},
+	{
+	  id: 2,
+	  name: 'NAISS2',
+	  image: NAISS_placeholder_img2,
+	  website: 'https://nittanyai.psu.edu/',
+	},
+];
 
 const Sponsors = () => {
 	return (
@@ -6,6 +26,13 @@ const Sponsors = () => {
 			<div className="w-4/12 flex flex-col items-center">
 				<h1 className="font-bold text-6xl">Sponsors</h1>
 				<Divider />
+				<div className="flex gap-8">
+					{sponsors.map((sponsor) => (
+						<a key={sponsor.id} href={sponsor.website} target="_blank" rel="noopener noreferrer" className="w-100 h-50">
+							<Image src={sponsor.image} alt={sponsor.name} width={200} height={100} objectFit="contain" />
+						</a>
+					))}
+				</div>
 			</div>
 		</section>
 	);
