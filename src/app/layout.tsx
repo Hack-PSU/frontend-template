8dfa47c993d7fb595fd5bef7b7d31244f8b66395
ChaseRensberger <chasehrensberger@gmail.com>
2023-03-24 22:45:40 -0400

    setting up more auth stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const path = usePathname();
+	const routesWithoutNavbar = ["signup", "profile"];
+
 	return (
 		<html lang="en">
 			{/*
@@ -20,10 +22,14 @@ export default function RootLayout({
       */}
 			<head />
 			<body>
-				{/* <FirebaseProvider auth={auth}> */}
-				{path === "/signup" ? <></> : <Navbar />}
-				{children}
-				{/* </FirebaseProvider> */}
+				<FirebaseProvider auth={auth}>
+					{routesWithoutNavbar.includes(path?.slice(1) ?? "") ? (
+						<></>
+					) : (
+						<Navbar />
+					)}
+					{children}
+				</FirebaseProvider>
 			</body>
 		</html>
 	);
