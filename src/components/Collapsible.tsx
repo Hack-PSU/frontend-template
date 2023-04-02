880554a32e0ec564086092950960d8f1b2dd1180
aaronfalk25 <aaronfalk25@gmail.com>
2023-04-02 18:46:45 -0400

    copied over framer motion animation from main

--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -2,66 +2,46 @@ import * as Collapsible from "@radix-ui/react-collapsible";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import { useState } from "react";
-import styled, { css, keyframes } from "styled-components";
-
-const slideDown = keyframes`
-  from {
-    height: 0;
-  }
-  to {
-    height: var(--radix-collapsible-content-height);
-  }
-`;
-
-const slideUp = keyframes`
-  from {
-    height: var(--radix-collapsible-content-height);
-  }
-  to {
-    height: 0;
-  }
-`;
-
-const CollapsibleContent = styled(Collapsible.Content)`
-  overflow: hidden;
-  width: 340px;
-
-  ${(props) =>
-    props["data-state"] === "open" &&
-    css`
-      animation: ${slideDown} 300ms ease-out;
-    `}
-
-  ${(props) =>
-    props["data-state"] === "closed" &&
-    css`
-      animation: ${slideUp} 300ms ease-out;
-    `}
-`;
+import { motion, AnimatePresence } from "framer-motion";
 
 const CCollapsible = (props: any) => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
+	const [isCollapsed, setIsCollapsed] = useState(true);
 
-  const toggleCollapsed = () => {
-    setIsCollapsed(!isCollapsed);
-  };
+	const toggleCollapsed = () => {
+		setIsCollapsed(!isCollapsed);
+	};
 
-  return (
-    <Collapsible.Root className="bg-black p-4 rounded-sm">
-      <Collapsible.Trigger
-        className="flex flex-row gap-8 text-white font-bold"
-        onClick={toggleCollapsed}
-      >
-        <p>{props.title}</p>
-        {isCollapsed ? <FaPlus size={20} /> : <FaMinus size={20} />}
-      </Collapsible.Trigger>
-      <CollapsibleContent
-        data-state={isCollapsed ? "closed" : "open"}
-      >
-        <div className="text-white pt-4">{props.content}</div>
-      </CollapsibleContent>
-    </Collapsible.Root>
-  );
+	const contentVariants = {
+		open: { opacity: 1, height: "auto" },
+		closed: { opacity: 0, height: 0 },
+	};
+
+	return (
+		<Collapsible.Root className="bg-black p-4 rounded-sm">
+			<Collapsible.Trigger
+				className="flex flex-row gap-8 text-white font-bold"
+				onClick={toggleCollapsed}
+			>
+				<p>{props.title}</p>
+				{isCollapsed ? <FaPlus size={20} /> : <FaMinus size={20} />}
+			</Collapsible.Trigger>
+			<AnimatePresence>
+				{!isCollapsed && (
+					<motion.div
+						initial="closed"
+						animate="open"
+						exit="closed"
+						variants={contentVariants}
+						transition={{ duration: 0.3 }}
+					>
+						<Collapsible.Content>
+							<p className="text-white pt-4 w-80">{props.content}</p>
+						</Collapsible.Content>
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</Collapsible.Root>
+	);
 };
 
 export default CCollapsible;
