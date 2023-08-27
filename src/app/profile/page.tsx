6a516987c8f567e1a2b3aaabdf94a4dc6b441d3e
ChaseRensberger <chasehrensberger@gmail.com>
2023-08-26 23:40:03 -0400

    add basic profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import QRCode from "react-qr-code";
+
+export default function Example() {
+	return (
+		<>
+			<div className="mx-auto max-w-7xl pt-16 lg:flex lg:gap-x-16 lg:px-8">
+				<main className="px-4 py-16 sm:px-6 lg:flex-auto lg:px-0 lg:py-20">
+					<div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
+						<div>
+							<h2 className="text-base font-semibold leading-7 text-gray-900">
+								Profile
+							</h2>
+							<p className="mt-1 text-sm leading-6 text-gray-500">
+								Edit the information you share with HackPSU.
+							</p>
+
+							<dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
+								<div className="pt-6 sm:flex">
+									<dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+										Full name
+									</dt>
+									<dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+										<div className="text-gray-900">Tom Cook</div>
+										<button
+											type="button"
+											className="font-semibold text-indigo-600 hover:text-indigo-500"
+										>
+											Update
+										</button>
+									</dd>
+								</div>
+								<div className="pt-6 sm:flex">
+									<dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+										Email
+									</dt>
+									<dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+										<div className="text-gray-900">tom.cook@example.com</div>
+										<button
+											type="button"
+											className="font-semibold text-indigo-600 hover:text-indigo-500"
+										>
+											Update
+										</button>
+									</dd>
+								</div>
+								<div className="pt-6 sm:flex">
+									<dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+										Password
+									</dt>
+									<dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+										<div className="text-gray-900">•••••••••••</div>
+										<button
+											type="button"
+											className="font-semibold text-indigo-600 hover:text-indigo-500"
+										>
+											Update
+										</button>
+									</dd>
+								</div>
+							</dl>
+						</div>
+
+						<div>
+							<h2 className="text-base font-semibold leading-7 text-gray-900">
+								QR Code
+							</h2>
+							<p className="mt-1 text-sm leading-6 text-gray-500">
+								Use this code to sign in to the event.
+							</p>
+							<QRCode value="hackpsu.org" className="" />
+						</div>
+					</div>
+				</main>
+			</div>
+		</>
+	);
+}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,23 +2,29 @@
 import Image from "next/image";
 import Logo from "../../../public/HackPSUBWLogo1.png";
 import useScroll from "@/lib/hooks/use-scroll";
-// import SignInButton from "./SignInButton";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
 	const scrolled = useScroll(50);
+	const pathname = usePathname();
 
 	return (
 		<>
-			<div
-				className={`fixed top-0 w-full flex flex-row items-center p-2 px-16 justify-between ${
-					scrolled
-						? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-						: "bg-white/0"
-				} z-30 transition-all`}
-			>
-				<Image src={Logo} width={50} height={50} alt="logo" />
-				{/* <SignInButton /> */}
-			</div>
+			{pathname !== "/profilee" ? (
+				<>
+					<div
+						className={`fixed top-0 w-full flex flex-row items-center p-2 px-16 justify-between ${
+							scrolled
+								? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+								: "bg-white/0"
+						} z-30 transition-all`}
+					>
+						<Image src={Logo} width={50} height={50} alt="logo" />
+					</div>
+				</>
+			) : (
+				<></>
+			)}
 		</>
 	);
 }
