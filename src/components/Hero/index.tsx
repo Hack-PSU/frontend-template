4de4e3aa694790686e9655577d7f56931c4a3278
ChaseRensberger <chasehrensberger@gmail.com>
2023-08-27 10:02:58 -0400

    add FadeIn component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Logo from "../../../public/HackPSUBWLogo1.png";
+import { FadeIn } from "../common/Fadein";
 
 const Hero = () => {
 	return (
-		<section className="flex flex-col items-center justify-center w-full h-[35rem]">
-			<Image src={Logo} width={250} height={250} alt="logo" />
-			<h1 className="font-bold text-6xl">HackPSU</h1>
-			<h2 className="font-bold text-2xl text-center">
-				Pennsylvania State University • April 1-2, 2023
-			</h2>
-		</section>
+		<FadeIn>
+			<section className="flex flex-col items-center justify-center w-full h-[35rem]">
+				<Image src={Logo} width={250} height={250} alt="logo" />
+				<h1 className="font-bold text-6xl">HackPSU</h1>
+				<h2 className="font-bold text-2xl text-center">
+					Pennsylvania State University • April 1-2, 2023
+				</h2>
+			</section>
+		</FadeIn>
 	);
 };
 
