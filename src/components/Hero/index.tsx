721b9adda8b30cf9fa563bb774de314c33cf77c4
ChaseRensberger <chasehrensberger@gmail.com>
2023-08-30 21:16:37 -0400

    add settings.json

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Logo from "../../../public/HackPSUBWLogo1.png";
-import { FadeIn } from "../common/Fadein";
+import settings from "@/lib/config/settings.json";
+import { Button } from "../common/Button";
 
 const Hero = () => {
 	return (
 		<section className="flex flex-col items-center justify-center w-full h-[35rem]">
 			<Image src={Logo} width={250} height={250} alt="logo" />
 			<h1 className="font-bold text-6xl">HackPSU</h1>
-			<h2 className="font-bold text-2xl text-center">
+			<h2 className="font-bold text-2xl text-center mb-4">
 				Pennsylvania State University • April 1-2, 2023
 			</h2>
+			{settings.isLive ? <h2>12 Hours Left</h2> : <></>}
 		</section>
 	);
 };
