d406e92e1a30624fdb4eabd13d6db0b954d0a976
Aaron Falk <aaronfalk25@gmail.com>
2023-10-11 09:06:25 -0400

    minor changes and added favicon

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
 				<div className="flex flex-col gap-4">
 					<CountdownTimer />
 
-					<div className="bg-white opacity-90 rounded-md p-2">
+					<div className="bg-sky-500 opacity-90 rounded-md p-2">
 						<h2 className="font-bold text-2xl text-center">
 							Pennsylvania State University • {settings.hackathonDateRepr}
 						</h2>
@@ -31,11 +31,21 @@ const Hero = () => {
 					<div>
 						{!isAuthenticated ? (
 							<div className="flex flex-col items-center justify-center">
-								<div className="w-1/2 h-1/2 p-4">
-									<BigButton background={Register}></BigButton>
+								<div className="w-3/5 h-1/2 p-4">
+									<BigButton
+										background={Register}
+										onClick={() => {
+											window.location.href = "/register";
+										}}
+									></BigButton>
 								</div>
-								<div className="w-1/2 h-1/2 p-4">
-									<BigButton background={Discord}></BigButton>
+								<div className="w-3/5 h-1/2 p-4">
+									<BigButton
+										background={Discord}
+										onClick={() => {
+											window.open("http://discord.hackpsu.org", "_blank");
+										}}
+									></BigButton>
 								</div>
 							</div>
 						) : (
