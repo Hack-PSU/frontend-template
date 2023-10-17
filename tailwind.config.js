d7264ccbdb7abc2278df9b39c063db04263bb054
John Durrell <john.m.durrell@gmail.com>
2023-10-17 16:31:43 -0400

    Google font capabilty.

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,9 +56,6 @@ module.exports = {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
 			},
-			fontFamily: {
-				Lato: ["Georgia", "sans-serif"],
-			},
 		},
 	},
 	content: [
