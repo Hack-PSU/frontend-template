33577980be23e652d1b645145669b124b827d101
John Durrell <john.m.durrell@gmail.com>
2023-10-18 21:02:23 -0400

    Update page metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
-import Navbar from "@/components/Navbar";
-import "@/styles/globals.css";
 import { Inter } from "next/font/google";
+
+import Navbar from "@/components/Navbar";
 import LayoutProvider from "@/lib/providers/LayoutProvider";
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "HackPSU Template",
-	description: "description",
+	title: "HackPSU Fall 2023",
+	description: "Main website for HackPSU",
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
