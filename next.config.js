ff2d86db9df3ead9a596ffcd227277c1805be7e7
Aaron Falk <aaronfalk25@gmail.com>
2024-02-24 10:03:22 -0500

    pushing updated next config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-      // TODO: Set up Next.js SVG handling.
-      // {
-      //   protocol: "https",
-      //   hostname: "tools.applemediaservices.com",
-      //   pathname: "/api/badges/download-on-the-app-store/**"
-      // },
-      {
-        protocol: "https",
-        hostname: "play.google.com",
-        pathname: "/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-      }
-    ]
-  }
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "firebasestorage.googleapis.com",
+			},
+			// TODO: Set up Next.js SVG handling.
+			// {
+			//   protocol: "https",
+			//   hostname: "tools.applemediaservices.com",
+			//   pathname: "/api/badges/download-on-the-app-store/**"
+			// },
+			{
+				protocol: "https",
+				hostname: "play.google.com",
+				pathname:
+					"/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
+			},
+		],
+		domains: ["firebasestorage.googleapis.com", "storage.googleapis.com"],
+	},
 };
 
 module.exports = nextConfig;
