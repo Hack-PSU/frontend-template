05a863092f040425f50d4c46cc46f957dc792b10
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2024-03-09 15:21:07 -0500

    new discord button

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,53 +1,59 @@
-import Image from 'next/image';
-import Logo from '../../../public/logo.png';
-import Register from '@/../public/images/TEXTBOX_register.png';
-import Discord from '@/../public/images/TEXTBOX_discord.png';
+import Image from "next/image";
+import Logo from "../../../public/logo.png";
+import Register from "@/../public/images/TEXTBOX_register.png";
+import Discord from "@/../public/images/TEXTBOX_discord.png";
 
-import settings from '@/lib/config/settings.json';
-import { useFirebase } from '@/lib/providers/FirebaseProvider';
-import CountdownTimer from './CountdownTimer';
-import BigButton from '@/components/common/BigButton';
+import settings from "@/lib/config/settings.json";
+import { useFirebase } from "@/lib/providers/FirebaseProvider";
+import CountdownTimer from "./CountdownTimer";
+import BigButton from "@/components/common/BigButton";
 
 const Hero = () => {
-    const { isAuthenticated } = useFirebase();
+	const { isAuthenticated } = useFirebase();
 
-    return (
-        <section id="hero" className="flex flex-col items-center justify-center w-4/5 h-[35rem] mt-8 mb-8">
-            <div className="frame flex p-8">
-                <div className="hidden md:block">
-                    <Image src={Logo} width={500} height={500} alt="logo" />
-                </div>
-                <div className="flex flex-col gap-4">
-                    <CountdownTimer />
+	return (
+		<section
+			id="hero"
+			className="flex flex-col items-center justify-center w-4/5 h-[35rem] mt-8 mb-8"
+		>
+			<div className="frame flex p-8">
+				<div className="hidden md:block">
+					<Image src={Logo} width={500} height={500} alt="logo" />
+				</div>
+				<div className="flex flex-col gap-4">
+					<CountdownTimer />
 
-                    <div className="bg-sky-500 opacity-90 rounded-md p-2">
-                        <h2 className="font-bold text-2xl text-center cornerstone-font">
-                            Penn State University • Business Building • {settings.hackathonDateRepr}
-                        </h2>
-                        {settings.isLive && <h2>12 Hours Left</h2>}
-                    </div>
+					<div className="bg-sky-500 opacity-90 rounded-md p-2">
+						<h2 className="font-bold text-2xl text-center cornerstone-font">
+							Penn State University • Business Building •{" "}
+							{settings.hackathonDateRepr}
+						</h2>
+						{settings.isLive && <h2>12 Hours Left</h2>}
+					</div>
 
-                    {!isAuthenticated && (
-                        <div className="flex flex-col items-center justify-center">
-                            <div className="sm:w-3/5 p-4 flex flex-wrap justify-center">
-                                <BigButton
-                                    background={Register}
-                                    onClick={() => window.open('https://register.hackpsu.org/register', '_blank')}
-                                    className="mb-4 w-full h-fit"
-                                />
+					<div className="flex flex-col items-center justify-center">
+						<div className="sm:w-3/5 p-4 flex flex-wrap justify-center">
+							<BigButton
+								background={Register}
+								onClick={() =>
+									window.open("https://register.hackpsu.org/register", "_blank")
+								}
+								className="mb-4 w-full h-fit"
+							/>
 
-                                <BigButton
-                                    background={Discord}
-                                    onClick={() => window.open('http://discord.hackpsu.org', '_blank')}
-                                    className="mb-4 w-full h-fit"
-                                />
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </section>
-    );
+							<BigButton
+								background={Discord}
+								onClick={() =>
+									window.open("http://discord.hackpsu.org", "_blank")
+								}
+								className="mb-4 w-full h-fit"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
