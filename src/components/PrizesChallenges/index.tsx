a98ce2ad814a18214a874d553fa4d01bf3c6fca6
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2024-10-11 22:37:00 -0400

    temp commit

--- src/components/PrizesChallenges/index.tsx
+++ src/components/PrizesChallenges/index.tsx
@@ -1,97 +1,170 @@
 import React from "react";
 import Divider from "../common/Divider";
-import "./PrizesChallenges.css";
 
 // Main Component
 const PrizesChallenges: React.FC = () => {
-	return (
-		<section id="prizes" className="flex flex-col items-center w-full mt-20">
-			<div className="w-full max-w-6xl flex flex-col items-center">
-				<h1 className="section-header-text">Prizes & Challenges</h1>
-				<Divider />
-				<div className="w-full grid md:grid-cols-1 lg:grid-cols-2 gap-8 mt-4">
-					<AwardBox
-						title="HackPSU Grand Prize"
-						description="The standard HackPSU experience: work together alone or in a team to build something awesome! All monetary prizes will be split among the winning team members equally."
-						prizes={[
-							{ place: "1st Place", amount: "$300" },
-							{ place: "2nd Place", amount: "$150" },
-							{ place: "3rd Place", amount: "$100" },
-						]}
-						extra="Winners will also receive MLH winner pins."
-					/>
-					<AwardBox
-						title="Sustainability Track"
-						description="Awarded to the team that demonstrates the most innovative use of technology to promote sustainability and environmental stewardship. A total of $150 will be distributed as Amazon gift cards, divided equally among the members of the winning team."
-					/>
-					<AwardBox
-						title="Generative AI Track"
-						description="Awarded to the hack with the best use of generative artificial intelligence. A total of $150 will be paid out via Amazon gift cards split evenly among the winning team members."
-					/>
-					<AwardBox
-						title="Peraton Award"
-						description="Awarded to the hack with the best use of React. Winners demonstrate proficiency in crafting user-friendly interfaces and dynamic applications. A total of $500 will be split evenly among the winning team members."
-					/>
-					<AwardBox
-						title="Entrepreneurship Award"
-						description="Awarded to the hack with the most sustainable business model, including considerations of monetization, target audience, and future growth. A total of $750 will be paid out via Amazon gift cards split evenly among the winning team members."
-						extra=""
-					/>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section id="prizes" className="flex flex-col items-center w-full mt-20">
+      <div className="w-full max-w-6xl flex flex-col items-center">
+        <h1 className="section-header-text">Prizes & Challenges</h1>
+        <Divider />
+        <div className="w-full grid md:grid-cols-1 lg:grid-cols-2 gap-8 mt-4">
+          <AwardBox
+            title="HackPSU Grand Prize"
+            description="The standard HackPSU experience: work together alone or in a team to build something awesome! All monetary prizes will be split among the winning team members equally."
+            prizes={[
+              { place: "1st Place", amount: "$300" },
+              { place: "2nd Place", amount: "$150" },
+              { place: "3rd Place", amount: "$100" },
+            ]}
+            extra="Winners will also receive MLH winner pins."
+          />
+
+          {/* Peraton Challenge */}
+          <AwardBox
+            title="Peraton Challenge"
+            description="Create the best web hack using React. Judging will consider overall user experience and UI design."
+            prizes={[
+              {
+                place: "Prize",
+                amount:
+                  "Peraton Backpack with Swag and $100 Amazon Gift Card for each team member",
+              },
+            ]}
+          />
+
+          {/* Smeal Challenge */}
+          <AwardBox
+            title="Smeal Challenge"
+            description="Build a platform that helps students identify steps to achieve their career goals based on educational backgrounds."
+            prizes={[
+              {
+                place: "Prize",
+                amount: "$300 in gift cards split among team members",
+              },
+            ]}
+          />
+
+          {/* Snap Challenge */}
+          <AwardBox
+            title="Snap Challenge"
+            description="Create a Lens on Spectacles that pushes the boundaries of wearable AR. Showcase innovative ways Spectacles can enhance real-world experiences."
+            prizes={[
+              { place: "1st Place", amount: "Nintendo Switch" },
+              { place: "2nd Place", amount: "Beats Solo Buds" },
+              { place: "3rd Place", amount: "Alexa Power Bank" },
+            ]}
+          />
+
+          {/* ICDS Challenge */}
+          <AwardBox
+            title="ICDS Challenge"
+            description="Develop a RAG chatbot to answer questions based on the ICDS user guide. Judged on quiz performance and design principles."
+            prizes={[
+              {
+                place: "Prize",
+                amount:
+                  "Endorsement package including press release, social media promotion, LinkedIn endorsement, and presentation invite",
+              },
+            ]}
+          />
+
+          {/* Nittany AI Challenge */}
+          <AwardBox
+            title="Nittany AI Challenge"
+            description="Use AI/ML/GenAI to address problems in health, humanitarianism, education, or the environment."
+            prizes={[
+              {
+                place: "1st Place",
+                amount:
+                  "$99 Amazon gift card for each team member (up to 5 members)",
+              },
+              {
+                place: "2nd Place",
+                amount:
+                  "$50 Amazon gift card for each team member (up to 5 members)",
+              },
+              {
+                place: "3rd Place",
+                amount:
+                  "$25 Amazon gift card for each team member (up to 5 members)",
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 // Interfaces for props
 interface Prize {
-	place: string;
-	amount: string;
+  place: string;
+  amount: string;
 }
 
 interface AwardBoxProps {
-	title: string;
-	description: string;
-	prizes?: Prize[]; // Optional prizes prop explicitly typed as an array of Prize objects
-	extra?: string;
+  title: string;
+  description: string;
+  prizes?: Prize[];
+  extra?: string;
 }
 
 // AwardBox Component
 const AwardBox: React.FC<AwardBoxProps> = ({
-	title,
-	description,
-	prizes = [],
-	extra,
+  title,
+  description,
+  prizes = [],
+  extra,
 }) => {
-	return (
-		<div className="p-3 award-box">
-			<div className="text-box">
-				<h2 className="text-3xl md:text-4xl font-bold text-center mb-2 text-white">
-					{title}
-				</h2>
-				<p className="text-base md:text-lg text-white mb-2">{description}</p>
+  return (
+    <div className="relative w-full h-[600px]">
+      <img
+        src="/HACKPSU2024_BOOTH-removebg.png"
+        alt="Booth"
+        className="w-full h-full object-cover"
+      />
 
-				{/* Prizes Table */}
-				{prizes.length > 0 && (
-					<table className="table-auto mt-2 text-white mx-auto">
-						<tbody>
-							{prizes.map((prize, index) => (
-								<tr key={index}>
-									<td className="text-base md:text-lg px-4">{prize.place}:</td>
-									<td className="text-base md:text-lg">{prize.amount}</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				)}
+      {/* Top Box - Description */}
+      <div className="absolute inset-x-0 top-[120px] flex flex-col items-center p-4">
+        <div className="bg-black/70 p-4 rounded-[8px] max-w-[80%] mx-auto text-white">
+          <p className="font-noto-serif text-base md:text-xl mb-4">
+            {description}
+          </p>
 
-				{/* Extra Info */}
-				{extra && (
-					<div className="text-base md:text-lg text-white mt-2">{extra}</div>
-				)}
-			</div>
-		</div>
-	);
+          {/* Prizes Table */}
+          {prizes.length > 0 && (
+            <table className="table-auto mt-4 mx-auto">
+              <tbody>
+                {prizes.map((prize, index) => (
+                  <tr key={index}>
+                    <td className="text-base md:text-xl px-4">
+                      {prize.place}:
+                    </td>
+                    <td className="text-base md:text-xl">{prize.amount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+
+          {/* Extra Info */}
+          {extra && (
+            <div className="text-base md:text-xl mt-4">{extra}</div>
+          )}
+        </div>
+      </div>
+
+      {/* Bottom Box - Title */}
+      <div className="absolute inset-x-0 bottom-0 flex justify-center p-4">
+        <div className="bg-black/70 py-2 px-4 rounded-t-[8px] text-white max-w-[80%] mx-auto">
+          <h2 className="font-rye text-2xl md:text-5xl font-bold text-center">
+            {title}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PrizesChallenges;
