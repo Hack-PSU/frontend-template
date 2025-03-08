3d26a9defe4753fa04231bbc70394e8e5c9506bc
Aaren Pan <aarenpan@Aarens-MacBook-Air.local>
2025-03-07 21:16:19 -0500

    Changed submission box border color from green to red

--- src/components/common/Submissions/index.tsx
+++ src/components/common/Submissions/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Submissions: React.FC = () => {
 	return (
-		<div className="w-11/12 md:w-5/12 p-3 bg-[rgba(0,0,0,0.75)] border-[green] border-4 rounded-lg text-center mt-20">
+		<div className="w-11/12 md:w-5/12 p-3 bg-[rgba(0,0,0,0.75)] border-[red] border-4 rounded-lg text-center mt-20">
 			<p className="text-lg text-left text-white">
 				<span style={{ color: "red", fontWeight: "bold", fontSize: "larger" }}>
 					SUBMISSIONS:
