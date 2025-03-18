0026040310f050e07d47d8cfa223e2770d1d0ce4
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-03-18 02:42:15 -0400

    fix img now?

--- src/components/FerrisWheel/index.tsx
+++ src/components/FerrisWheel/index.tsx
@@ -2,67 +2,65 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import base from "@/public/Base.svg";
+import wheel from "@/public/Wheel.svg";
 
 // Wheel SVG with visible spokes.
 function WheelSVG() {
-  return (
-	<img src="/wheel.svg" alt="Ferris Wheel" width={500} height={500}  />
-  );
+	return <img src={wheel} alt="Ferris Wheel" width={500} height={500} />;
 }
 
 // Base SVG for the Ferris Wheel.
 function BaseSVG() {
-  return (
-	<img src="/base.svg" alt="Ferris Wheel Base" width={300} height={300} />
-  );
+	return <img src={base} alt="Ferris Wheel Base" width={300} height={300} />;
 }
 
 const FerrisWheel = () => {
-  const [wheelRotation, setWheelRotation] = useState(0);
+	const [wheelRotation, setWheelRotation] = useState(0);
 
-  // Handle scroll wheel events to update the rotation.
-  const handleWheel = (e: React.WheelEvent) => {
-    e.preventDefault();
-    setWheelRotation((prev) => prev + e.deltaY);
-  };
+	// Handle scroll wheel events to update the rotation.
+	const handleWheel = (e: React.WheelEvent) => {
+		e.preventDefault();
+		setWheelRotation((prev) => prev + e.deltaY);
+	};
 
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        overflow: "hidden", // Hide the scrollbar.
-        position: "relative",
-      }}
-      onWheel={handleWheel}
-	  className="mt-[-200px]"
-    >
-      {/* Rotating wheel */}
-      <motion.div
-        style={{
-          rotate: wheelRotation,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
-        <WheelSVG />
-      </motion.div>
+	return (
+		<div
+			style={{
+				width: "100%",
+				height: "100vh",
+				overflow: "hidden", // Hide the scrollbar.
+				position: "relative",
+			}}
+			onWheel={handleWheel}
+			className="mt-[-200px]"
+		>
+			{/* Rotating wheel */}
+			<motion.div
+				style={{
+					rotate: wheelRotation,
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					height: "100%",
+				}}
+			>
+				<WheelSVG />
+			</motion.div>
 
-      <div
-        style={{
-          position: "absolute",
-          bottom: 0,
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <BaseSVG />
-      </div>
-    </div>
-  );
+			<div
+				style={{
+					position: "absolute",
+					bottom: 0,
+					width: "100%",
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<BaseSVG />
+			</div>
+		</div>
+	);
 };
 
 export default FerrisWheel;
