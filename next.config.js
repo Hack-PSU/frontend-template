2de980e68684287952db84f94b54b24daffd7f51
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-03-18 02:18:25 -0400

    fix svg?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		dangerouslyAllowSVG: true, // Apple App Store badge is SVG.
+		dangerouslyAllowSVG: true,
+		contentDispositionType: 'attachment',
+    	contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 		remotePatterns: [
 			{
 				protocol: "https",
