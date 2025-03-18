d51bbf724f1d2b4cd08beb0395b893d9851b4f80
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-03-18 02:27:31 -0400

    try this

--- src/components/FerrisWheel/index.tsx
+++ src/components/FerrisWheel/index.tsx
@@ -6,14 +6,14 @@ import Image from "next/image";
 // Wheel SVG with visible spokes.
 function WheelSVG() {
   return (
-	<Image src="/wheel.svg" alt="Ferris Wheel" width={500} height={500} unoptimized={true} />
+	<img src="/wheel.svg" alt="Ferris Wheel" width={500} height={500}  />
   );
 }
 
 // Base SVG for the Ferris Wheel.
 function BaseSVG() {
   return (
-	<Image src="/base.svg" alt="Ferris Wheel Base" width={300} height={300} unoptimized={true} />
+	<img src="/base.svg" alt="Ferris Wheel Base" width={300} height={300} />
   );
 }
 
